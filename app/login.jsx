9f07670bcf0787a908842b5c187a544f0d7810be
Dipanshu Rawat <dipanshurawat4@gmail.com>
2025-01-14 02:26:19 +0530

    working on  login and signup pages

--- app/login.jsx
+++ app/login.jsx
@@ -1,14 +1,64 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import ScreenWrapper from '../components/ScreenWrapper'
+import Home from '../assets/icons/Home'
+import { theme } from '../constants/theme'
+import Icon from '../assets/icons'
+import { StatusBar } from 'expo-status-bar'
+import BackButton from '../components/BackButton'
+import { useRouter } from 'expo-router'
+import { hp, wp } from '../helpers/common'
+
+
+
 
 const Login=()=> {
+  const router = useRouter();
   return (
-    <View>
-      <Text>Login</Text>
+    <ScreenWrapper>
+    <StatusBar style="dark" />
+    <View style={styles.container}>
+      <BackButton router={router}/> 
+
+      {/* welcome text */}
+      <Text style={styles.welcomeText}>Hey,</Text>
+      <Text style={styles.welcomeText}>Welcome Back</Text>
     </View>
+    </ScreenWrapper>
   )
 }
 
-export default Login
+export default Login;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+container: {
+    flex:1,
+    gap:45,
+    paddingHorizontal: wp(5)
+},
+welcomeText:{
+    fontSize: hp(4),
+    fontWeight: theme.fonts.bold,
+    color:theme.colors.text,
+},
+form: {
+    gap: 25,
+},
+forgotPassword:{
+    textAlign:'right',
+    fontWeight: theme.fonts.semibold,
+    color: theme.fonts.semibold,
+    color: theme.colors.text
+},
+footer:{
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap:5
+},
+footerText: {
+    textAlign: 'center',
+    color: theme.colors.text,
+    fontSize: hp(1.6)
+}
+})
