497f5c9f86768837166514398b10017a861f72d1
Dipanshu Rawat <dipanshurawat4@gmail.com>
2025-01-18 19:41:01 +0530

    update

---
+++ app/(main)/home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import ScreenWrapper from '../../components/ScreenWrapper'
+
+function Home() {
+  return (
+    <ScreenWrapper>
+<Text>hello</Text>
+    </ScreenWrapper>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({})
--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,8 +1,35 @@
-import { Stack } from 'expo-router'
-import React from 'react'
-import { View, Text } from 'react-native'
+import { Stack, useRouter } from 'expo-router'
+import React, { useEffect } from 'react'
+import { AuthProvider, useAuth } from './context/AuthContext'
+import { supabase } from '../lib/supabase'
 
-function _layout() {
+const _layout = ()=> {
+  return (
+    <AuthProvider>
+      <MainLayout />
+    </AuthProvider>
+  )
+}
+
+function MainLayout() {
+  const {setAuth} = useAuth();
+  const router = useRouter();
+
+useEffect(()=>{
+  supabase.auth.onAuthStateChange((_event, session) => {
+    console.log('session user ',session?.user?.id);
+
+    if (session){
+      setAuth(session?.user);
+      router.replace('/home');
+    }else{
+      setAuth(null);
+      router.replace('/welcome');
+    }
+    
+
+  })
+},[])
   return (
     <Stack
       screenOptions={{
